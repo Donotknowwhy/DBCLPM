83ce4cfe257668d865b8a07918a466717ebe7ce1
Xuân Kiên <kienlovend@gmail.com>
2021-05-25 15:57:25 +0700

    add login

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,29 +1,110 @@
-import { Layout, Row, Col, Card } from "antd";
-import React from "react";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import SocialLogin from "./../../components/SocialLogin";
-import styles from "./index.module.scss";
-import { getAccessToken } from "./../../utils/cookies";
-export default function PublicLayout() {
-  const router = useRouter();
-  const accessToken = getAccessToken();
+import {Form, Input, Button, Checkbox, Row, Col} from 'antd';
+import {useRouter} from 'next/router';
+import SocialLogin from '../../components/SocialLogin';
+import React, {useState} from 'react';
+import {signIn} from '../../api/firebase-client';
+import styles from './index.module.scss';
+import PublicLayout from '../../layouts/PublicLayout';
+import Link from 'next/link';
 
-  useEffect(() => {
-    if (accessToken) router.push("/");
-  }, []);
+function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const login = () => signIn(email, password)
+      .then(() => {
+        router.push('/');
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        const errorMessage = error.message;
+        // ...
+        window.alert('Error' + errorMessage);
+      });
 
   return (
-    <>
-      <Layout className={styles.cover}>
-        <Row justify="center">
-          <Card 
-          hoverable
-          className={styles.card}>
-            <SocialLogin />
-          </Card>
+    <PublicLayout>
+      <div>
+        <h2 className={styles.formTitle}>Login</h2>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{remember: true}}
+          onFinish={(e) => e.preventDefault() && false}
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {required: true, message: 'Please input your Email!'},
+            ]}
+          >
+            <Input
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {required: true, message: 'Please input your Password!'},
+            ]}
+          >
+            <Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Row className={styles.formContainer}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                block
+                onClick={login}
+              >
+                Log in
+              </Button>
+            </Row>
+          </Form.Item>
+        </Form>
+        <Row>
+          <Col span={12}>
+            <Link href="/#" variant="body2">
+              <a>Forgot password?</a>
+            </Link>
+          </Col>
+          <Col span={12}>
+            <div>
+              <span className={styles.formReg}>
+                {'Don\'t have an account? '}
+              </span>
+              <Link href="/#" variant="body2">
+                <a>Register</a>
+              </Link>
+            </div>
+          </Col>
         </Row>
-      </Layout>
-    </>
+        <Row className={styles.formContainer}>
+          <div className={styles.formDivSpan}>
+            <span className={styles.formSpan}>
+              Or
+            </span>
+          </div>
+        </Row>
+        <SocialLogin/>
+      </div>
+    </PublicLayout>
   );
 }
+
+export default Login;
